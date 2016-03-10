b1d68f1224f3ceedc2a7ca5162a5b70aee2bb833
Erik <moteer@bitbucket.org>
2016-03-11 08:35:31 +1100

    added more complex table content test

--- src/main/webapp/scripts/test/app/EditableRoutePartTableWidgetTest.js
+++ src/main/webapp/scripts/test/app/EditableRoutePartTableWidgetTest.js
@@ -43,4 +43,53 @@ describe('EditTableController simple Unit Test', function () {
     });
 });
 
+describe('EditTableController with complex row content', function () {
+    beforeEach(module('editableRoutePartTableApp'));
+
+    var EditableRoutePartTableController,
+        scope;
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        EditableRoutePartTableController = $controller('EditableRoutePartTableController', {
+            $scope: scope
+        });
+    }));
+
+    it('adds complex object to row', function () {
+        var complexObj = {lat:'123', lng:456};
+        expect(scope.elements()).toBe(0);
+        scope.newRow = complexObj;
+        scope.addRow();
+        expect(scope.rows).toContain(complexObj);
+        expect(scope.elements()).toBe(1);
+
+        var anotherComplexObj = {lat:'999', lng:555};
+
+        scope.newRow = anotherComplexObj;
+        scope.addRow();
+        expect(scope.rows).toContain(complexObj);
+        expect(scope.rows).toContain(anotherComplexObj);
+        expect(scope.elements()).toBe(2);
+    });
+
+    it('removes Elements from rows', function () {
+        scope.rows = ["one", "two", "three"];
+        expect(scope.elements()).toBe(3);
+
+        scope.deleteRow("two");
+        expect(scope.elements()).toBe(2);
+        expect(scope.rows).toEqual(["one", "three"]);
+
+        scope.deleteRow("three");
+        expect(scope.elements()).toBe(1);
+        expect(scope.rows).toEqual(["one"]);
+
+        scope.deleteRow("three");
+        expect(scope.elements()).toBe(1);
+        expect(scope.rows).toEqual(["one"]);
+    });
+});
+
+
 
