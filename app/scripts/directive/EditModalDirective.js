97aece893c537a527391b9195c352d20c39e6164
Erik Hölzel (taahoer2) <erik.hoelzel@swisscom.com>
2017-07-14 16:18:08 +0200

    reorganises source tree

--- app/scripts/directive/EditModalDirective.js
+++
@@ -1,9 +0,0 @@
-mapApp.directive('editModal', function () {
-        return {
-            templateUrl:'partials/templates/edit-modal-content.html',
-            restrict: 'E',
-            transclude: true,
-            replace: true,
-        }
-    }
-);
